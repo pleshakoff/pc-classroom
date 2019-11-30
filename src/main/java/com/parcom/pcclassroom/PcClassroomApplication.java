895cbbad7d3a85a70e66ef45a2c741715e0edfd6
pleshakoff <pleshakoff@gmail.com>
2019-11-30 10:19:03 +0300

    init

---
+++ src/main/java/com/parcom/pcclassroom/PcClassroomApplication.java
@@ -0,0 +1,13 @@
+package com.parcom.pcclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcClassroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcom/pcclassroom/PcClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcom.pcclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
