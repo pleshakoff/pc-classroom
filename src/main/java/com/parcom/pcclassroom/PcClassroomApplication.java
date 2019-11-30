7254dea7fda8969ed2913414a5ad8165d525ce5d
pleshakoff <pleshakoff@gmail.com>
2019-11-30 12:28:21 +0300

    Подключил postgres

--- src/main/java/com/parcom/pcclassroom/PcClassroomApplication.java
+++ src/main/java/com/parcom/pcclassroom/PcClassroomApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PcClassroomApplication {
 
+//	docker run --rm --name classroom_pg -e POSTGRES_PASSWORD=parcom -d -p 5432:5432 postgres
 	public static void main(String[] args) {
 		SpringApplication.run(PcClassroomApplication.class, args);
 	}
