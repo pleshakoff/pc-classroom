560acba5cf99566d1b8399417ef6c450242fa326
pleshakoff <pleshakoff@gmail.com>
2019-11-30 18:49:05 +0300

    Инициализация с пользователем.Рефакторинг

--- src/main/java/com/parcom/classroom/model/user/UserRegisterByGroupDTO.java
+++ src/main/java/com/parcom/classroom/model/user/UserRegisterByGroupDTO.java
@@ -1,36 +1,19 @@
 package com.parcom.classroom.model.user;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
+
 @Getter
-@Builder
-public class UserRegisterByGroupDTO {
+public class UserRegisterByGroupDTO extends UserRegisterDTO {
 
-    private String username;
-    private String firstName;
-    private String middleName;
-    private String familyName;
-    private String email;
-    private String phone;
-    private String password;
-    private String passwordConfirm;
-    private Long groupId;
+    @NotNull
+    private final Long groupId;
 
-    @JsonCreator
     public UserRegisterByGroupDTO(String username, String firstName, String middleName, String familyName, String email, String phone, String password, String passwordConfirm, Long groupId) {
-        this.username = username;
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.familyName = familyName;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.passwordConfirm = passwordConfirm;
+        super(username, firstName, middleName, familyName, email, phone, password, passwordConfirm);
         this.groupId = groupId;
     }
-
-
 }
---
+++ src/main/java/com/parcom/classroom/model/user/UserRegisterDTO.java
@@ -0,0 +1,50 @@
+package com.parcom.classroom.model.user;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+public class UserRegisterDTO {
+
+    @NotNull
+    protected final String username;
+
+    @NotNull
+    protected final String firstName;
+
+    protected final String middleName;
+
+    @NotNull
+    protected final String familyName;
+
+    @NotNull
+    protected final String email;
+
+    @NotNull
+    protected final String phone;
+
+    @NotNull
+    protected final String password;
+
+    @NotNull
+    protected final String passwordConfirm;
+
+
+    @JsonCreator
+    public UserRegisterDTO(String username, String firstName, String middleName, String familyName, String email, String phone, String password, String passwordConfirm) {
+        this.username = username;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.familyName = familyName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+
+    }
+
+
+}
