f8dadb40c7758477289467399cd67e74eb807319
pleshakoff <pleshakoff@gmail.com>
2020-01-18 01:33:23 +0300

    Перешел на новую версию стартера секюрити

--- src/main/java/com/parcom/classroom/ClassroomApplication.java
+++ src/main/java/com/parcom/classroom/ClassroomApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -17,13 +19,13 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @EnableAsync
+@EnableWebSecurity()
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class ClassroomApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClassroomApplication.class, args);
 	}
-
-
 
 }
 
--- src/main/java/com/parcom/classroom/SecurityConfiguration.java
+++ src/main/java/com/parcom/classroom/SecurityConfiguration.java
@@ -2,7 +2,12 @@ package com.parcom.classroom;
 
 
 import com.parcom.security_client.AuthenticationTokenProcessingFilter;
+import com.parcom.security_client.ParcomWebSecurityConfigurerAdapter;
 import com.parcom.security_client.UnauthorizedEntryPoint;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,38 +19,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 
 @Configuration
-@EnableWebSecurity()
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends ParcomWebSecurityConfigurerAdapter {
 
-
-    private final  UnauthorizedEntryPoint unauthorizedEntryPoint;
-
-    private final AuthenticationTokenProcessingFilter authenticationTokenProcessingFilter;
-
-    public SecurityConfiguration(UnauthorizedEntryPoint unauthorizedEntryPoint, AuthenticationTokenProcessingFilter authenticationTokenProcessingFilter) {
-        this.unauthorizedEntryPoint = unauthorizedEntryPoint;
-        this.authenticationTokenProcessingFilter = authenticationTokenProcessingFilter;
+    public SecurityConfiguration() {
+        super();
+        permitAllList.add("/add/**");
     }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/webjars/springfox-swagger-ui/**",
-                "/swagger-ui.html/**",
-                "/swagger-resources/**",
-                "/v2/api-docs",
-                 "/health",
-                "/add/**").
-                 permitAll()
-                .anyRequest().authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint)
-                .and()
-                .addFilterBefore(authenticationTokenProcessingFilter, UsernamePasswordAuthenticationFilter.class);
-
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
-    }
-
 
 }
